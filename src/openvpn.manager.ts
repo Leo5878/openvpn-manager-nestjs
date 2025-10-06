7e7e5ea3f9f1c62ad0d6c108809be8051d43c99d
Leo5878 <leonardo5878@yandex.ru>
2025-10-07 00:34:26 +0300

    try/catch when is not connected to the server

--- src/openvpn.manager.ts
+++ src/openvpn.manager.ts
@@ -1,12 +1,20 @@
 import { Inject, Injectable, OnApplicationBootstrap } from "@nestjs/common";
 import { OpenvpnService } from "@app/openvpn-mngr-ts/openvpn-manager-nestjs/openvpn-mngr-ts.service";
+import { Logger } from "nestjs-pino";
 
 @Injectable()
 export class OpenvpnManager implements OnApplicationBootstrap {
-  constructor(@Inject() private clients: OpenvpnService) {}
+  constructor(
+    @Inject() private clients: OpenvpnService,
+    @Inject() private readonly logger: Logger,
+  ) {}
   async onApplicationBootstrap() {
     for (const c of this.clients.instances.values()) {
-      await c.connect();
+      try {
+        await c.connect();
+      } catch (e) {
+        this.logger.error(e);
+      }
     }
   }
   async onModuleDestroy() {
