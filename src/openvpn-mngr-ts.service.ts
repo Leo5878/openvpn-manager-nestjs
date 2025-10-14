f92b2b4c7f7716312f2c6b2400878513e55eb980
Leo5878 <leonardo5878@yandex.ru>
2025-10-15 01:21:54 +0300

    Rename class

--- src/openvpn-mngr-ts.service.ts
+++ src/openvpn-mngr-ts.service.ts
@@ -1,6 +1,6 @@
 import { Injectable, Inject } from "@nestjs/common";
 import { EventEmitter } from "node:events";
-import { OpenvpnApi } from "@app/openvpn-mngr-ts/openvpn-manager/openvpn-api";
+import { OpenvpnManager } from "@app/openvpn-mngr-ts/openvpn-manager/openvpn-manager";
 import { Connect } from "@app/openvpn-mngr-ts/openvpn-manager/core";
 import { OPENVPN_OPTIONS } from "./openvpn.constants";
 import { CustomEventType } from "@openvpn-manager/event-responses.types";
@@ -17,7 +17,7 @@ export interface OpenvpnServiceOptions {
 // TODO rename to manager
 @Injectable()
 export class OpenvpnService {
-  public instances: Map<string, OpenvpnApi> = new Map();
+  public instances: Map<string, OpenvpnManager> = new Map();
   public emitter: EventEmitter<CustomEventType>;
 
   constructor(
@@ -30,13 +30,16 @@ export class OpenvpnService {
     this.options.connections.forEach(({ id, host, port }) =>
       this.instances.set(
         id,
-        new OpenvpnApi({ id, host, port }, { emitter, logger: this.logger }),
+        new OpenvpnManager(
+          { id, host, port },
+          { emitter, logger: this.logger },
+        ),
       ),
     );
   }
 
   // метод для доступа к конкретному клиенту, если понадобится
-  getClient(id: string): OpenvpnApi | undefined {
+  getClient(id: string): OpenvpnManager | undefined {
     return this.instances.get(id);
   }
 }
