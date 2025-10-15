bf56dfba50a535517829e8ad1ee2b55811ba0437
Leo5878 <leonardo5878@yandex.ru>
2025-10-16 02:36:32 +0300

    Update package and build

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
 export { registerOpenvpnListeners } from "./listener-register";
 export { OpenvpnService } from "./openvpn-mngr-ts.service";
 export { OpenvpnEvent } from "./event-decorators";
+export { OpenvpnModuleImpl } from "./openvpn-mngr-ts.module"
